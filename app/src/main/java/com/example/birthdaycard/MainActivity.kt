e569c9435ce7d9a6361e0044be3850a907161352
shashikant010 <shashikantyadav0503@gmail.com>
2024-10-24 20:41:03 +0530

    done with happy birthday text

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
@@ -20,7 +27,7 @@ class MainActivity : ComponentActivity() {
             BirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    GreetingText(message = "happy birthday sky", from = "from sk")
+                    GreetingText(message = "happy birthday sky", from = "from sk",modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -29,16 +36,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String,from: String,modifier: Modifier=Modifier){
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp,
-
-    )
+    Column(modifier=modifier.padding(8.dp), verticalArrangement = Arrangement.Center) {
+        Text(
+            text = message,
+            fontSize = 90.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier=modifier.padding(16.dp).align(Alignment.End)
+            )
+    }
 
     /*
     * Row(
